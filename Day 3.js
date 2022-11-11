23cb0dff0c38d8e5f5c4715ca174be9577eb8b3e
Prabhaharan1392 <115686911+Prabhaharan1392@users.noreply.github.com>
2022-11-11 10:00:42 +0530

    Add files via upload

---
+++ Day 3.js
@@ -0,0 +1,15 @@
+var resume = [{
+
+"name" : "prabhaharan",
+"qualification" : "ME",
+"place" : "nagercoil",
+"work" : "full stack devoleper"
+},
+{ 
+
+"internships" : "ITC limited",
+"skills" : "javascript" 
+
+
+}]
+console.log(resume)
